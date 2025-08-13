9f77f9001829a3cbe8d6d29957b502339b95f5ff
Drikus Roor <drikusroor@gmail.com>
2025-08-13 21:47:31 +0200

    feat: set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/picturific-pdf-image-extractor/',
   server: { port: 5173 },
   build: {
     rollupOptions: {},
